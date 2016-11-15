8eadcdcf3e95d2538c599433d2113c70ad713115
Cassidy Brooke <cassidy@cassidybrooke.net>
2016-11-14 20:14:07 -0500

    Create tic-tac-toe.py

---
+++ tic-tac-toe.py
@@ -0,0 +1,35 @@
+#! /usr/bin/python3
+
+
+# To play, run program in Terminal then enter your choice of
+# position surrounded in single quotes. For example: 'mid-L'.
+# Forgetting the single quotes will cause the game to crash.
+
+# This is a primitive rendition of tic-tac-toe.
+
+theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
+            'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
+            'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
+
+def printBoard(board):
+    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
+    print('-+-+-')
+    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
+    print('-+-+-')
+    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
+
+turn = 'X'
+for i in range(9):
+    printBoard(theBoard)
+    print('Turn for ' + turn + '.  Move on which space?')
+    move = input()
+    theBoard[move] = turn
+    if turn == 'X':
+        turn = 'O'
+    else:
+        turn = 'X'
+
+
+
+
+printBoard(theBoard)
