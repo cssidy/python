a0912e347c447bc1597ef4048fdceacaacf08ba2
Cassidy Brooke <cassidy@cassidybrooke.net>
2016-12-02 18:11:10 -0500

    Create magic8Ball2.py

---
+++ automate-the-boring-stuff-with-python/magic8Ball2.py
@@ -0,0 +1,16 @@
+#! Python3
+# The same as the original magic8Ball.py except uses a more elegant list.
+
+import random
+
+messages = ['It is certain',
+            'It is decidedly so',
+            'Yes definitely',
+            'Reply hazy try again',
+            'Ask again later',
+            'Concentrate and ask again',
+            'My reply is no',
+            'Outlook not so good',
+            'Very doubtful']
+
+print(messages[random.randint(0, len(messages) - 1)])
