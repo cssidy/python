d1b6e33d2d012bdd17354f58f7e49352d30190d1
cssidy <cassidy@cassidybrooke.net>
2016-12-08 17:04:28 -0500

    Added more to regex.py

--- automate-the-boring-stuff-with-python/regex.py
+++ automate-the-boring-stuff-with-python/regex.py
@@ -6,4 +6,13 @@ phoneNumRegex = re.compile(r' \d\d\d-\d\d\d-\d\d\d\d')
 mo = phoneNumRegex.search('My number is 415-555-4242.')
 print('Phone number found: ' + mo.group())
 
+heroRegex = re.compile(r'Batman|Tina Fey')
+mo1 = heroRegex.search('Batman and Tina Fey.')
+mo2 = heroRegex.search('Tina Fey and Batman.')
+print(mo1.group())
+print(mo2.group())
 
+batRegex = re.compile(r'Bat(man|mobile|copter|bat)')
+mo = batRegex.search('Batmobile lost a wheel.')
+print(mo.group())
+print(mo.group(1))
