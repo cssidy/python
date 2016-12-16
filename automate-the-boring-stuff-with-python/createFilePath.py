348fab5c0026060be720249fe3f6fa62fdeaafc8
cssidy <cassidy@cassidybrooke.net>
2016-12-15 21:01:06 -0500

    Update createFilePath.py

--- automate-the-boring-stuff-with-python/createFilePath.py
+++ automate-the-boring-stuff-with-python/createFilePath.py
@@ -6,3 +6,7 @@ myFiles = ['accounts.txt', 'details.csv', 'invite.docx']
 
 for filename in myFiles:
     print(os.path.join('/Users/cassidy', filename))
+
+print(os.getcwd())
+os.chdir('/home/cassidy/Documents')
+print(os.getcwd())
