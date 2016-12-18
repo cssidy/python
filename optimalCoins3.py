8986ba7426b3bf3a78ed2bef43eeb202c484e8fa
cssidy <cassidy@cassidybrooke.net>
2016-12-17 22:27:26 -0500

    Fix indentation

--- optimalCoins3.py
+++ optimalCoins3.py
@@ -4,29 +4,29 @@
 
 
 def dp_make_change(coin_value_list, change, min_coins, coins_used):
-   for cents in range(change+1):
-      coin_count = cents
-      new_coin = 1
-      for j in [c for c in coin_value_list if c <= cents]:
+    for cents in range(change+1):
+        coin_count = cents
+        new_coin = 1
+        for j in [c for c in coin_value_list if c <= cents]:
             if min_coins[cents-j] + 1 < coin_count:
-               coin_count = min_coins[cents-j]+1
-               new_coin = j
-      min_coins[cents] = coin_count
-      coins_used[cents] = new_coin
-   return min_coins[change]
+                coin_count = min_coins[cents-j]+1
+                new_coin = j
+        min_coins[cents] = coin_count
+        coins_used[cents] = new_coin
+    return min_coins[change]
 
 
 def print_coins(coins_used, change):
-   coin = change
-   while coin > 0:
-      this_coin = coins_used[coin]
-      print(this_coin)
-      coin = coin - this_coin
+    coin = change
+    while coin > 0:
+        this_coin = coins_used[coin]
+        print(this_coin)
+        coin = coin - this_coin
 
 
 def main():
     amnt = 63
-    c_list = [1, 5, 10, 21, 25]
+    c_list = [1, 5, 10, 25]
     coins_used = [0]*(amnt+1)
     coin_count = [0]*(amnt+1)
 
@@ -34,8 +34,8 @@ def main():
     print(dp_make_change(c_list, amnt, coin_count, coins_used),"coins")
     print("They are:")
     print_coins(coins_used, amnt)
-    print("The used list is as follows:")
-    print(coins_used)
+    # print("The used list is as follows:")
+    # print(coins_used)
 
 main()
 
