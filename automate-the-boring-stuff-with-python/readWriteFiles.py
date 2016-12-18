75abb5c763c4f4e339d540c9ad1bbacc6035fcda
cssidy <cassidy@cassidybrooke.net>
2016-12-17 23:02:30 -0500

    Explore write() method

--- automate-the-boring-stuff-with-python/readWriteFiles.py
+++ automate-the-boring-stuff-with-python/readWriteFiles.py
@@ -12,3 +12,18 @@ print(helloFile, helloContent)
 sonnetFile = open(path + '/automate-the-boring-stuff-with-python/sonnet29.txt')
 print(sonnetFile.readlines())
 
+baconFile = open('bacon.txt', 'w')
+baconFile.write('Hello world!\n')
+# 13
+
+baconFile.close()
+baconFile = open('bacon.txt', 'a')
+baconFile.write('Bacon is not a vegetable.')
+# 25
+
+baconFile.close()
+baconFile = open('bacon.txt')
+content = baconFile.read()
+print(content)
+# Hello world!
+# Bacon is not a vegetable.
