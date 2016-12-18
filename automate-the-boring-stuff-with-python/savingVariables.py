b485d263b3a766ee5e531d22172045a1ed5d17fd
cssidy <cassidy@cassidybrooke.net>
2016-12-17 23:12:42 -0500

    Pretty print

---
+++ automate-the-boring-stuff-with-python/savingVariables.py
@@ -0,0 +1,24 @@
+#! Python3
+
+import shelve
+import pprint
+
+shelfFile = shelve.open('mydata')
+cats = ['Zophie', 'Pooka', 'Simon']
+shelfFile['cats'] = cats
+shelfFile.close()
+
+shelfFile = shelve.open('mydata')
+type(shelfFile)
+shelfFile['cats']
+shelfFile.close()
+
+list(shelfFile.keys())
+list(ShelfFile.values())
+shelfFile.close
+
+cats = [{'name': 'Zophie', 'desc': 'chubby'}, {'name': 'Pooka', 'desc': 'fluffy'}]
+pprint.pformat(cats)
+fileObj = open('myCats.py', 'w')
+fileObj.write('cats = ' + pprint.pformat(cats) + '\n')
+fileObj.close()
