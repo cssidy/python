0a5a5835bb2c609705bf4eb661fa8aa8bc3bd3ab
cssidy <cassidy@cassidybrooke.net>
2016-12-20 23:46:53 -0500

    Experiment with memoization

--- project-euler/longestCollatzSequence.py
+++ project-euler/longestCollatzSequence.py
@@ -1,18 +1,20 @@
 #! Python
 
+memo = {}
+
 
 def collatz_sequence(number):
-    memo = {}
+    if n not in memo:
+
     while number != 1:
         if number % 2 == 0:
-            print(number / 2)
             number /= 2
-            memo[x] = number
+            return memo[number]
         elif number % 2 == 1:
-            print(3 * number + 1)
             number = 3 * number + 1
-            memo[x] = number
+            return memo[number]
         else:
             break
-    print(memo)
-collatz_sequence(1000000)
+
+collatz_sequence(100)
+print(memo)
