d17bbcb267a54a5d35e8896784bcde18ecc68b12
cssidy <cassidy@cassidybrooke.net>
2016-12-20 23:53:27 -0500

    Create new function to update inventory

--- automate-the-boring-stuff-with-python/fantasyGameInventory.py
+++ automate-the-boring-stuff-with-python/fantasyGameInventory.py
@@ -2,6 +2,7 @@
 
 stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
 
+
 def display_inventory(inventory):
     print('Inventory:')
     item_total = 0
@@ -11,3 +12,12 @@ def display_inventory(inventory):
     print('Total number of items: ' + str(item_total))
 
 display_inventory(stuff)
+
+
+def add_to_inventory(inventory, added_items):
+    
+
+inv = {'gold coin': 42, 'rope': 1)
+dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+inv = add_to_inventory(inv, dragon_loot)
+display_inventory(inv)
