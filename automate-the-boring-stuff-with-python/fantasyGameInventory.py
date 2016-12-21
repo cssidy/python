5b154f8e43828c993202741d8915bd43bce34606
cssidy <cassidy@cassidybrooke.net>
2016-12-21 00:20:28 -0500

    Solution to fantasyGameInventory.py

--- automate-the-boring-stuff-with-python/fantasyGameInventory.py
+++ automate-the-boring-stuff-with-python/fantasyGameInventory.py
@@ -11,13 +11,14 @@ def display_inventory(inventory):
         item_total += v
     print('Total number of items: ' + str(item_total))
 
-display_inventory(stuff)
-
 
 def add_to_inventory(inventory, added_items):
-    
+    for item in added_items:
+        inventory.setdefault(item, 0)
+        inventory[item] += + 1
+    return inv
 
-inv = {'gold coin': 42, 'rope': 1)
+inv = {'gold coin': 42, 'rope': 1}
 dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
 inv = add_to_inventory(inv, dragon_loot)
 display_inventory(inv)
