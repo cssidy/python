15e9a88f56f43244a0c0a9ad150b6913ecfff1aa
Cassidy Brooke <cassidy@cassidybrooke.net>
2017-10-11 21:05:29 -0400

    Create madLibs.py

---
+++ independent-study/madLibs.py
@@ -0,0 +1,15 @@
+#! Python3
+# Reads in text files and lets the user add their own text anywhere the word ADJECTIVE, NOUN, ADVERB, or VERB appears
+# in the text file. Print results to the screen and save to a new text file.
+
+noun = input('Enter a noun: ')
+pronoun = input('Enter a pronoun: ')
+adjective = input('Enter an adjective: ')
+adverb = input('Enter an adverb: ')
+verb = input('Enter a verb: ')
+
+# open txt file
+
+# create a new txt file
+
+print(lib)
