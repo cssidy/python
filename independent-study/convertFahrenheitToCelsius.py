54e32c409e98c2f308efdc12fae99e3b74cdf557
Cassidy Brooke <cassidy@cassidybrooke.net>
2017-10-11 21:01:47 -0400

    Create convertFahrenheitToCelsius.py

---
+++ independent-study/convertFahrenheitToCelsius.py
@@ -0,0 +1,7 @@
+#! python3
+# convert degrees Fahrenheit to Celsius
+
+
+ftemp = int(input('Enter the temperature, in degrees Fahrenheit: '))
+ctemp = (ftemp - 32) * 5/9
+print('The temperature in degrees Celsius is: ', ctemp) 
