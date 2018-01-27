bf5246b4e3a61ea796a827cb2a64c8bd190f19e3
cssidy <cassidy@cassidybrooke.net>
2018-01-27 16:59:09 -0500

    reading and writing csv files

---
+++ Automate The Boring Stuff With Python/removeCSVHeader.py
@@ -0,0 +1,31 @@
+#! python3
+
+import csv
+import os
+
+os.makedirs('headerRemoved', exist_ok=True)
+
+# Loop through every file in the current working directory
+for csvFilename in os.listdir('.'):
+    if not csvFilename.endswith('.csv'):
+        continue
+
+    print('Removing header from ' + csvFilename + '...')
+
+    # read the csv file in (skipping first row)
+    csvRows = []
+    csvFileObj = open(csvFilename)
+    readerObj = csv.reader(csvFileObj)
+    for row in readerObj:
+        if readerObj.line_num == 1:
+            continue
+        csvRows.append(row)
+    csvFileObj.close()
+
+    # write out the new csv file
+    csvFileObj = open(os.path.join('headerRemoved', csvFilename), 'w', newline='')
+    csvWriter = csv.writer(csvFileObj)
+    for row in csvRows:
+        csvWriter.writerow(row)
+    csvFileObj.close()
+    
