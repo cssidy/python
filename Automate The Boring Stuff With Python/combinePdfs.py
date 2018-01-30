bad31d15281e30968b4b3d2e6a02cd74fb05a3ae
cssidy <cassidy@cassidybrooke.net>
2018-01-30 07:46:36 -0500

    combining pdfs

---
+++ Automate The Boring Stuff With Python/combinePdfs.py
@@ -0,0 +1,29 @@
+#! python3
+
+import PyPDF2
+import os
+
+
+# get all pdf file names
+pdfFiles = []
+for filename in os.listdir('.'):
+    if filename.endswith('.pdf'):
+        pdfFiles.append(filename)
+
+pdfFiles.sort(key=str.lower)
+
+pdfWriter = PyPDF2.PdfFileWriter()
+
+# loop through all the pdf files
+for filename in pdfFiles:
+    pdfFileObj = open(filename, 'rb')
+    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+    # loop through all the pages (except the first) and add them
+    for pageNum in range(1, pdfReader.numPages):
+        pageObj = pdfReader.getPage(pageNum)
+        pdfWriter.addPage(pageObj)
+
+# save the resulting pdfs to a new file
+pdfOutput = open('allminutes.pdf', 'wb')
+pdfWriter.write(pdfOutput)
+pdfOutput.close()
